35e960b76d5c5c9c613aeefd408c60e90b5e2a47
TMaropela <t.maropela@gmail.com>
2024-01-17 03:44:57 -0800

    12

---
+++ 0x0D-SQL_introduction/12-no_cheating.sql
@@ -0,0 +1,4 @@
+--Write a script that updates the score of Bob to 10 in the table second_table.
+--You are not allowed to use Bob’s id value, only the name field
+
+UPDATE `second table` SET score = “10” WHERE name = Bob;
