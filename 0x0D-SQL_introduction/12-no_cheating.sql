b99bafa837e5646e485f5c744424755c4192d4e2
TMaropela <t.maropela@gmail.com>
2024-01-17 04:02:53 -0800

    12 to advanced

--- 0x0D-SQL_introduction/12-no_cheating.sql
+++ 0x0D-SQL_introduction/12-no_cheating.sql
@@ -1,4 +1,4 @@
---Write a script that updates the score of Bob to 10 in the table second_table.
---You are not allowed to use Bob’s id value, only the name field
-
-UPDATE `second table` SET `score` = 10 WHERE `name` = "Bob";
+-- Updates the score of Bob to 10 in the table second_table in my MySQL server.
+UPDATE `second_table`
+SET `score` = 10
+WHERE `name` = "Bob";
